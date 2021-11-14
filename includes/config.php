15ad99c4c31786d248bf1b4701c7e4329802fab8
Atryom <238136@mail.ru>
2021-11-14 22:03:13 +0300

    Initial commit

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+  const SITE_NAME = 'Twitter';
+  const HOST = "http://localhost/twitter";
+  const DB_HOST = 'localhost';
+  const DB_NAME = 'twitter';
+  const DB_USER = 'root';
+  const DB_PASS = 'root';
+?>
